2e34e4d66f79e7aaae8ee035d6f1e91cddc15336
Cody Blakeney <codyblakeney@Codys-MacBook-Pro.local>
2016-02-10 10:47:09 -0600

    added function definitions needed for project. Wrote functions for finding and printing hailstone sequences.

--- program2/program2.cpp
+++ program2/program2.cpp
@@ -3,7 +3,56 @@
 
 using namespace std;
 
+vector<int> findHailstone(int);
+void 		printHailstone(vector<int>&);
+int 		hailstoneLength(vector<int>&);
+int 		returnLargest(vector<int>&);
+int 		returnSmallest(vector<int>&);
+int 		evenCount(vector<int>&);
+int 		oddCount(vector<int>&);
+
 int main(){
 
+	int n = 0;
+
+	cout << "please enter an integer: ";
+	cin >> n;
+
+	vector<int> hailstone = findHailstone(n);
+
+	printHailstone(hailstone);
+
 	return 0;
+}
+
+vector<int> findHailstone(int n){
+
+	vector<int> hailstone;
+
+	hailstone.push_back(n);
+
+	while(n != 1){
+
+		if(n % 2 == 1){
+
+			n = n * 3 + 1;
+		}
+
+		else if(n % 2 == 0){
+
+			n /= 2;
+		}
+
+		hailstone.push_back(n);
+	}
+
+	return hailstone;
+}
+
+void printHailstone(vector<int> & hailstone){
+
+	for(int i = 0; i < hailstone.size(); i++){
+
+		cout << hailstone.at(i) << " ";
+	}
 }
