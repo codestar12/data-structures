9a082c318ddd479de1afec4df5b61ab13f37dc13
Cody Blakeney <codyblakeney@Codys-MBP.attlocal.net>
2016-03-27 17:04:46 -0500

    added check to see if stack and queue are the same. just need to validate input. need to make sure string ends with ;

--- program5/program5.cpp
+++ program5/program5.cpp
@@ -69,12 +69,11 @@ class Stack{
 int main(){
 
     string input1, input2;
-    Stack myStack;
+    Stack myStack, reverseStack;
     Queue myQueue;
     int choice;
-
+    
     do{
-
         cout << "*** Welcome to My stack / Queue Program ***\n";
         cout << "The function of this program is validate that\n";
         cout << "a stack and a queue are identical.\n";
@@ -84,7 +83,7 @@ int main(){
         cout << "corresponding positions are the same.\n\n";
         cout << "Select from the following menu\n";
         cout << "1.   Enter Stack / Queue Values.\n";
-        cout << "9.   Terminate the program.    1\n";
+        cout << "9.   Terminate the program.    \n";
 
         do{
             cin >> choice;
@@ -98,6 +97,8 @@ int main(){
             if(choice != 1 && choice != 9)
                 cout << "Invalid selection. Please select 1 or 9\n";
         }while(choice != 1 && choice != 9);
+
+        bool same = true;
 
         if(choice == 1){
             cout << "Enter Stack Values terminated by ;    ";
@@ -116,9 +117,26 @@ int main(){
             for(int i = 0; i < input2.length(); i++){
                 if(input2[i] != ';'){
                     value = int(input2[i]) - 48; // 48 is ascii value for 0
-                    myStack.push(value);
+                    myQueue.push_back(value);
                 }
-            }        
+            } 
+
+            if(myStack.returnSize() == myQueue.returnSize()){
+                while(!myStack.empty()){
+                    reverseStack.push(myStack.pop());
+                }
+                while(!reverseStack.empty()){
+                    if(reverseStack.pop() != myQueue.pop_front())
+                        same = false;
+                }
+            }
+            else
+                same = false;  
+
+            if(same)
+                cout << "\nStack and Queue are identical\n";
+            else
+                cout << "\nStack and Queue are not identical\n";         
         }
         cout << endl << endl; 
     }while(choice != 9);  
