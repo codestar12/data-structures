065e627f5618b8abe2719fabdcb2e2eed36ab310
Cody Blakeney <codyblakeney@Codys-MBP.attlocal.net>
2016-03-27 16:06:06 -0500

    only thing left to add is to compare the stack and queue

--- program5/program5.cpp
+++ program5/program5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -67,26 +68,63 @@ class Stack{
 
 int main(){
 
-    Queue myQueue;
-    myQueue.push_back(5);
-    myQueue.push_back(4);
-    myQueue.push_back(3);
-    
+    string input1, input2;
     Stack myStack;
-    
-    myStack.push(5);
-    myStack.push(4);
-    myStack.push(3);
+    Queue myQueue;
+    int choice;
 
-    
-    cout << "Queue\n";
-    while(!myQueue.empty()){
-        cout << myQueue.pop_front() << endl;
-    }
+    do{
 
-    cout << "Stack\n";
-    while(!myStack.empty()){
-        cout << myStack.pop() << endl;
-    }
+        cout << "*** Welcome to My stack / Queue Program ***\n";
+        cout << "The function of this program is validate that\n";
+        cout << "a stack and a queue are identical.\n";
+        cout << "Stacks / queues are  same if\n";
+        cout << "they have the same number of elements\n";
+        cout << "and the their elements at the\n";
+        cout << "corresponding positions are the same.\n\n";
+        cout << "Select from the following menu\n";
+        cout << "1.   Enter Stack / Queue Values.\n";
+        cout << "9.   Terminate the program.    1\n";
+
+        do{
+            cin >> choice;
+            while(cin.fail()){ //makes sure an integer was the input
+            cin.clear();
+            cout << "You did not enter an integer. "; 
+            cout << "You will be returned to the menu after the"<<endl;
+            cout<<" next question."<<endl;
+            cin.ignore(256, '\n');
+            }
+            if(choice != 1 && choice != 9)
+                cout << "Invalid selection. Please select 1 or 9\n";
+        }while(choice != 1 && choice != 9);
+
+        if(choice == 1){
+            cout << "Enter Stack Values terminated by ;    ";
+            cin >> input1;
+            cout << "Enter Queue Values terminated by ;    ";
+            cin >> input2;
+            int value;
+
+            for(int i = 0; i < input1.length(); i++){
+                if(input1[i] != ';'){
+                    value = int(input1[i]) - 48; // 48 is ascii value for 0
+                    myStack.push(value);
+                }
+            }
+
+            for(int i = 0; i < input2.length(); i++){
+                if(input2[i] != ';'){
+                    value = int(input2[i]) - 48; // 48 is ascii value for 0
+                    myStack.push(value);
+                }
+            }        
+        }
+        cout << endl << endl; 
+    }while(choice != 9);  
+
+    cout << "*** Program is terminated." 
+         << "Written by Cody Blakeney and Chris McCarty ***";
+
     return 0;
 }
